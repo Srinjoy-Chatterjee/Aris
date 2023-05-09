924d9712c8d966f1c8a353d4a8e597a21084238c
Srinjoy-Chatterjee <87896364+Srinjoy-Chatterjee@users.noreply.github.com>
2023-05-10 02:00:26 +0530

    git lfs

---
+++ Assets/InfinityPBR/_InfinityPBR - Goblin/Scripts/GoblinDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoblinDemo : MonoBehaviour
+{
+
+    private Animator animator;
+    public Button[] textureButtons;
+    public Toggle[] wardrobeToggles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetLocomotion(float value)
+    {
+        animator.SetFloat("Locomotion", value);
+    }
+
+    public void SuperRandomize()
+    {
+        textureButtons[Random.Range(0, textureButtons.Length)].onClick.Invoke();
+        for (int i = 0; i < wardrobeToggles.Length; i++)
+        {
+            if (Random.Range(0, 2) == 1)
+            {
+                wardrobeToggles[i].isOn = true;
+            }
+            else
+            {
+                wardrobeToggles[i].isOn = false;
+            }
+        }
+    }
+}
