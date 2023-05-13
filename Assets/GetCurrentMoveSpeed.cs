8bd6def4422508a9ae5cec77342b7cafb3867152
Suvankar <49644381+Suvankar017@users.noreply.github.com>
2023-05-13 20:13:32 +0530

    Screwed up Enemy

---
+++ Assets/GetCurrentMoveSpeed.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GetCurrentMoveSpeed : MonoBehaviour
+{
+    public float CurrentSpeed;
+    public NavMeshAgent Agent;
+
+    public float Speed => CurrentSpeed;
+
+    private void Update()
+    {
+        CurrentSpeed = Agent.velocity.magnitude;
+    }
+}
